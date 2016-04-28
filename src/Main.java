e07ee3ec1aaa04bcfe5fb02d549fe20e3d338c5f
Dghelneshi <Dghelneshi@users.noreply.github.com>
2016-04-28 03:51:01 +0200

    camera switching, todo-list, small adjustments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -18,37 +20,62 @@ import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
 import org.opencv.videoio.VideoCapture;
 
+/* TODO:
+ *  - match eye template instead of moving with face
+ *  - look up parameters on detectMultiScale (rejectLevel, etc)
+ *  - try out more classifiers
+ *  - maybe delete face and/or eye area from image after each detection to avoid multiples 
+ *  - detect faces over 2-3 frames, choose only those which are in all frames, then track them over a longer period (1sec?)
+ *  - find a method for handling people moving out of camera fov while they are being tracked (re-detect on hitting edge?)
+ *  - fine-tune parameters according to available camera
+ */
+
 public class Main {
 
     public static boolean isRunning = true;
     public static Imshow  im;
-
+    public static VideoCapture vcam;
+    public static int cameraIndex = 0;
+    public static int numCameras = 2;
+        
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
         im = new Imshow("Video Preview");
-
         im.Window.setResizable(true);
-
-        Mat image = new Mat();
-        VideoCapture vcam = new VideoCapture(0);
-
+        
         im.Window.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 isRunning = false;
                 vcam.release(); // required to let java exit properly
             }
         });
-
-        // loop until VideoCamera is Available
-        while (vcam.isOpened() == false)
-            ;
+        
+        im.Window.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                vcam.release();
+                cameraIndex++;
+                cameraIndex = cameraIndex % numCameras;
+                vcam = new VideoCapture(cameraIndex);
+            }
+            public void mousePressed(MouseEvent e) {}
+            public void mouseReleased(MouseEvent e) {}
+            public void mouseEntered(MouseEvent e) {}
+            public void mouseExited(MouseEvent e) {}
+        });
+        
+        Mat image = new Mat();
+        vcam = new VideoCapture(cameraIndex);
+        
+        if (vcam.isOpened() == false)
+            return;
 
         // loop until image frames are not empty
         while (image.empty()) {
             vcam.read(image);
         }
-
+        
+        
         CascadeClassifier faceCascade = new CascadeClassifier("data/haarcascades/haarcascade_frontalface_alt.xml");
         CascadeClassifier eyeCascade = new CascadeClassifier("data/haarcascades/haarcascade_eye.xml");
 
@@ -62,28 +89,40 @@ public class Main {
         Mat hatimg = Imgcodecs.imread("data/hat2.png");
 
         int frameCounter = 0;
+        
         while (isRunning) {
-            vcam.read(image);
-
-            if (faces.isEmpty()) {
-                detectFaces(image, faces, faceCascade, eyeCascade, minFaceSize, minEyeSize);
+            
+            if (vcam.isOpened()) {
+            
+                vcam.read(image);
+    
+                System.out.println(image.rows() + "r");
+                System.out.println(image.cols() + "c");
+                
+                long t0 = System.nanoTime();
+                
+                if (faces.isEmpty()) {
+                    detectFaces(image, faces, faceCascade, eyeCascade, minFaceSize, minEyeSize);
+                }
+                else if ((frameCounter & 15) == 0) { // periodically reset faces, need something more sophisticated later on
+                    faces.clear();
+                    detectFaces(image, faces, faceCascade, eyeCascade, minFaceSize, minEyeSize);
+                }
+                else {
+                    trackFaces(image, faces);
+                }
+                
+                long t1 = System.nanoTime();
+                im.Window.setTitle("Camera: " + cameraIndex + " - " + ((t1 - t0) / 1000000.0f) + "ms");
+    
+                drawHats(image, faces, hatimg);
+                
+                im.showImage(image);
+    
+                frameCounter++;
             }
-            else if ((frameCounter & 15) == 0) { // periodically reset faces, need something more sophisticated later on
-                faces.clear();
-                detectFaces(image, faces, faceCascade, eyeCascade, minFaceSize, minEyeSize);
-            }
-            else {
-                trackFaces(image, faces);
-            }
-
-            drawHats(image, faces, hatimg);
-
-            im.showImage(image);
-
-            frameCounter++;
         }
         vcam.release();
-        System.exit(0);
     }
 
     public static void drawHats(Mat image, List<Face> faces, Mat hatimg) {
@@ -106,6 +145,8 @@ public class Main {
     }
 
     public static void trackFaces(Mat image, List<Face> faceList) {
+        
+        // NOTE: need to handle moving out of the frame
 
         Mat grayImage = new Mat();
 
@@ -133,11 +174,27 @@ public class Main {
             if (minMax.minVal <= 0.1) { // likely match
                 Face f = faceList.get(i);
 
-                f.faceRect.x = (int) (roi.x + minMax.minLoc.x);
-                f.faceRect.y = (int) (roi.y + minMax.minLoc.y);
+                int newFaceX = (int) (roi.x + minMax.minLoc.x);
+                int newFaceY = (int) (roi.y + minMax.minLoc.y);
+                
+                f.faceRect.x = newFaceX;
+                f.faceRect.y = newFaceY;
                 f.faceData = grayImage.submat(f.faceRect).clone();
 
-                Imgproc.rectangle(image, f.faceRect.tl(), f.faceRect.br(), new Scalar(0, 255, 255, 255), 3);
+                Imgproc.rectangle(image, f.faceRect.tl(), f.faceRect.br(), new Scalar(0, 255, 255, 255), 2);
+
+                for (int j = 0; j < f.eyeRects.size(); j++) {
+
+                    Rect eyeRect = f.eyeRects.get(j);
+                    
+                    Point eyeRelTl = eyeRect.tl();
+                    Point eyeRelBr = eyeRect.br();
+                    
+                    Point eyeAbsTl = new Point(eyeRelTl.x + newFaceX, eyeRelTl.y + newFaceY);
+                    Point eyeAbsBr = new Point(eyeRelBr.x + newFaceX, eyeRelBr.y + newFaceY);
+
+                    Imgproc.rectangle(image, eyeAbsTl, eyeAbsBr, new Scalar(255, 0, 255, 255), 1);
+                }
 
             }
             else { // not matched
@@ -157,7 +214,7 @@ public class Main {
 
         MatOfRect faceRects = new MatOfRect();
 
-        faceCascade.detectMultiScale(grayImage, faceRects, 1.1, 2, Objdetect.CASCADE_SCALE_IMAGE, new Size(minFaceSize, minFaceSize), new Size());
+        faceCascade.detectMultiScale(grayImage, faceRects, 1.1, 3, Objdetect.CASCADE_SCALE_IMAGE, new Size(minFaceSize, minFaceSize), new Size());
 
         Rect[] facesArray = faceRects.toArray();
         for (int i = 0; i < facesArray.length; i++) {
@@ -167,11 +224,11 @@ public class Main {
 
             Mat faceMat = grayImage.submat(facesArray[i]).clone();
 
-            Imgproc.rectangle(image, faceTl, faceBr, new Scalar(0, 255, 0, 255), 3);
+            Imgproc.rectangle(image, faceTl, faceBr, new Scalar(0, 255, 0, 255), 2);
 
             MatOfRect eyeRects = new MatOfRect();
 
-            eyeCascade.detectMultiScale(faceMat, eyeRects, 1.1, 10, Objdetect.CASCADE_SCALE_IMAGE, new Size(minEyeSize, minEyeSize), new Size());
+            eyeCascade.detectMultiScale(faceMat, eyeRects, 1.1, 5, Objdetect.CASCADE_SCALE_IMAGE, new Size(minEyeSize, minEyeSize), new Size());
 
             ArrayList<Mat> eyeMats = new ArrayList<Mat>();
 
@@ -188,7 +245,7 @@ public class Main {
 
                 eyeMats.add(grayImage.submat(eyeRect).clone());
 
-                Imgproc.rectangle(image, eyeAbsTl, eyeAbsBr, new Scalar(255, 0, 0, 255), 2);
+                Imgproc.rectangle(image, eyeAbsTl, eyeAbsBr, new Scalar(255, 0, 0, 255), 1);
             }
 
             Face f = new Face(facesArray[i], faceMat, eyesArray, eyeMats);
